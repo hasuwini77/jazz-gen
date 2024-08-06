84767f9bc4d80be4d2dd55b77768daf6943ae774
Edwin Andermyr <andermyredwin@gmail.com>
2024-08-06 11:18:36 +0200

    added basic routes and basic nav

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Link from "next/link";
+import styled from "styled-components";
+
+const NavbarContainer = styled.nav`
+  background-color: #333;
+  padding: 1rem;
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+  margin: 0;
+  padding: 0;
+`;
+
+const NavItem = styled.li`
+  color: #fff;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 1.2rem;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <NavList>
+        <NavItem>
+          <Link href="/">Home</Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/about">About</Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/contact">Contact</Link>
+        </NavItem>
+      </NavList>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <>
+      <main>
+        <Navbar />
+      </main>
+    </>
+  );
 }

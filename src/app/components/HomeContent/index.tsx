248ca1fa0503565388966e87169b5865f0261e81
Edwin Andermyr <andermyredwin@gmail.com>
2024-08-06 15:15:59 +0200

    added a paragraph

--- src/app/components/HomeContent/index.tsx
+++ src/app/components/HomeContent/index.tsx
@@ -1,15 +1,26 @@
+import styled from "styled-components";
+
 const HomeContent = () => {
+  const StyledTitle = styled.h3`
+    padding: 1rem;
+  `;
+
+  const StyledPara = styled.p`
+    padding: 0.8rem;
+    line-height: 1.6rem;
+  `;
   return (
     <>
       <div>
-        <h3> What is Jazzer Gen?</h3>
-        <p>
-          Thanks to the API hosted on binaryjazz.us. We are able to gather data
-          in order to create unique and creatire "Music genres" as well as
-          "Stories" made of their internal catalogs of each genre "fragment".
-          These catalogs are broken into instruments, beats, adjectives,
-          prefixes and suffixes, regions, and genres.
-        </p>
+        <StyledTitle> What is Jazzer Gen?</StyledTitle>
+        <StyledPara>
+          Thanks to the API hosted on
+          <a href="http://binaryjazz.us">binaryjazz.us</a>. We are able to
+          gather data in order to create unique and creatire "Music genres" as
+          well as "Stories" made of their internal catalogs of each genre
+          "fragment". These catalogs are broken into instruments, beats,
+          adjectives, prefixes and suffixes, regions, and genres.
+        </StyledPara>
       </div>
     </>
   );
